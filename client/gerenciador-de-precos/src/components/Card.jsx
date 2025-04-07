acd41f7d5695164b8a9a3c1ba86dfc7c960bf5da
joaogpf <jgpf-dark@hotmail.com>
2025-04-07 17:22:41 -0300

    aprimorando exibicao de precos

--- client/gerenciador-de-precos/src/components/Card.jsx
+++ client/gerenciador-de-precos/src/components/Card.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import axios from "axios"
 import './Card.css'
 
+
 import { useState, useEffect } from "react";
 
 const Card = () => {
 
     const [products, setProducts] = useState([])
+    const [prices, setPrices] = useState([])
     
 
     useEffect (() => {
@@ -14,21 +16,24 @@ const Card = () => {
         const getProducts = async () => {
             try {
                 const response = await axios.get("http://127.0.0.1:5000/products")
+                const responsePrices = await axios.get("http://127.0.0.1:5000/prices")
                 setProducts(response.data)
+                setPrices(responsePrices.data)
             } catch(error) {
                 console.error(error)
             }
         }
       
         getProducts()
-    }, [products])
+    }, [products, prices])
 
     return (
         <div className="cardsContainer">
             {products.map((product) => (
                 <div className="produto" key={product.id}> 
                     <div className="nome">{product.name}</div>
-                    <div className="preco">R$ {product.price}</div>
+                    <div className="preco-antigo">R$ {prices[(product.id-1)].old_price}</div>
+                    <div className="preco-atual">R$ {prices[(product.id-1)].new_price}</div>
                 </div>
             ))}
             
